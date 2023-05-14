4008561bba50ba5d3460eb42e2ab6b9dd1f4206f
3BoOoD97 <abodn70@hotmail.com>
2023-05-14 18:17:51 +0200

    Working on the Navbar

---
+++ home.js
@@ -0,0 +1,13 @@
+
+
+
+const navbar = document.getElementById("navbar")
+let top = navbar.offsetTop;
+function stickynavbar() {
+  if (window.scrollY >= top) {    
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');    
+  }
+}
+window.addEventListener('scroll', stickynavbar);
